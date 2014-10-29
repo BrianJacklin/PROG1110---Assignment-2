410484436ca1e16a755a0f49af352873489e6b2c
BJack <brianjacklinca@gmail.com>
2014-10-29 16:14:02 -0400

    window.onload

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-// total contributors
+window.onload = function(){
+	// total contributors
 	var totalCont = document.getElementById("contributions");
 	var rowCount = document.getElementById("lighthouse-table").rows.length - 1; 
 	totalCont.innerHTML = rowCount; 
 
-// total contributions
+	// total contributions
 	var myTable = document.getElementById("lighthouse-table");
 	var totalAmount = document.getElementById("totalAmount");
 	var amount = 0
@@ -13,3 +14,4 @@
 		amount += parseInt(myTable.rows[i].cells[1].innerHTML);
 		totalAmount.innerHTML = "$"+amount.toFixed(2);
 	}
+}
